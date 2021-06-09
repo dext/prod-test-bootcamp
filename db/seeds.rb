bbd63c2e643a86b312465bb8ef40fb0cb3de1c04
Stefan Kanev <stefan.kanev@gmail.com>
2021-06-09 12:38:53 +0300

    Add some basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create! email: 'john@example.com', password: '123456' # TODO use '1'?
+
+user.password = '1'
+user.password_confirmation = '1'
+user.save! validate: false
+
+a_team = Team.create! name: 'A-Team',     location: 'Accountantshire'
+b_team = Team.create! name: 'Team Bravo', location: 'Bravoshire'
+
+magneto = Client.create! name: 'Magneto', user_id: user.id, team_id: a_team.id, provider: 'Provider', business_type: 'partnership'
+charles = Client.create! name: 'Charles', user_id: user.id, team_id: a_team.id, provider: 'Provider', business_type: 'partnership'
+
+cyclops = Client.create! name: 'Cyclops', user_id: user.id, team_id: b_team.id, provider: 'Provider', business_type: 'partnership'
+jubilee = Client.create! name: 'Jubilee', user_id: user.id, team_id: b_team.id, provider: 'Provider', business_type: 'partnership'
+
+user.teams << a_team
+user.teams << b_team
